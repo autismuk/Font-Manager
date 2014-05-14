b959616b2eca70546ac622e46a03a4d10f42b5d6
Paul Robson <paul@robsons.org.uk>
2014-05-14 13:24:54 +0100

    Improved comments in main.lua bit out of date

--- main.lua
+++ main.lua
@@ -18,22 +18,24 @@ local msg = "Another line\rwith\ra curve" .. string.char(0xC3,0xBE,0x2A) 						-
 local str = fm.BitmapString:new("demofont") 													-- create a string OOP method.
 str:moveTo(160,240):setScale(2,2):setFontSize(50) 												-- centre it, double the scale, size 48.
 str:setText(msg)																				-- set the text
---str:setDirection(180)
-str:setAnchor(0.5,0)
+--str:setDirection(180) 																		-- write it backwards (why did I do this ?)
+str:setAnchor(0.5,0) 																			-- anchor top centre,
 str:setModifier("iscale")																		-- shape with a curve
 str:setVerticalSpacing(0.5):animate(4)															-- animate it - if you comment this out it will curve but not animate
+ 																								-- the number is a speed scalar.
 
 str2 = display.newBitmapText("Bye !",0,0,"font2",45) 											-- or we can do it Corona style !  - YAY !!!!
 																								-- *BUT* it does not have compatible methods. So you have to use moveTo()
 str2:setAnchor(0,0):setScale(-1,1):setDirection(270)											-- and setAnchor() for example, rather than accessing members directly.
 
-local str3 = fm.BitmapString:new("font2",28):													-- a third string, created using the constructor.
-								moveTo(160,400):setText("Wobbly text"):setScale(2,2)
+local str3 = fm.BitmapString:new("font2",28):													-- a third string, created using the constructor, showing chaining.
+								moveTo(160,400):setText("Wobbly text"):setScale(2,2) 			
 
--- str3:setModifier(fm.Modifiers.WobbleModifier:new(2))											-- modifier examples.
--- str3:setModifier(SimpleCurveModifier:new(0,180,4,2))
+
+-- str3:setModifier(fm.Modifiers.WobbleModifier:new(2))											-- a more violent wobble with a new wobble modifier instance
+-- str3:setModifier(SimpleCurveModifier:new(0,180,4,2)) 										-- simple curves and scales with a different part of the trigonometrical curve
 -- str3:setModifier(SimpleCurveScaleModifier:new(0,180,4,2))
-str3:setModifier("wobble")
+str3:setModifier("wobble") 																		-- or just wobble.
 str3:animate() 																					-- make this one animate.
 
 --
@@ -62,6 +64,7 @@ end
 
 --
 --	Identical but pulses words, so uses wordIndex and wordCount instead
+--
 --	tests wordCount because this could be zero, causing divide by zero.
 --
 function wordPulser(modifier, cPos, info)
@@ -74,37 +77,35 @@ function wordPulser(modifier, cPos, info)
 	end
 end
 
-str:setModifier(linePulser)
+-- str:setModifier(linePulser) 																	-- pick something to run.
 str:setModifier(wordPulser)
 
-local str4 = display.newBitmapText("pulse",160,240,"retrofont",80) 								-- create a new string
+local str4 = display.newBitmapText("pulse",160,240,"retrofont",80) 								-- create a new string using Corona method.
 str4:setModifier(pulser):animate() 																-- make it use the above modifier, and animate it.
--- str4:setDirection(180)
--- str4:setText("Hello\nWorld !")
 
-local demoTarget = {}
+-- str4:setDirection(180) 																		-- backwards
+-- str4:setText("Hello\nWorld !") 																-- you can change the text, strings aren't immutable.
+
+local demoTarget = {} 																			-- something to send an event to.
 function demoTarget.tap(event) print("tap",event) end
 
 str4:addEventListener( "tap", demoTarget )														-- print 'tap' if you tap it.
 -- str4:removeEventListener("tap")
 
-
--- str4:remove()
+-- str4:remove() 																				-- remove kills it. str4 will be an empty object.
 -- for _,n in pairs(str4) do print("str4",_,n) end
 
--- str:setModifier(pulser)
-local t = 1000 																					-- run over 8 seconds.
+local t = 8000 																					-- run over 8 seconds.
 
 --
 --	Animate using the usual Corona methods.
 --
---	Remove the comments, the screen clears at the end.
+--	Remove the comment, the screen clears at the end, this is a 'tidy everything up' routine.
 --
 
 transition.to(str:getView(),{ time = t,rotation = 0, y = 0, xScale = 0.5, yScale = 1, rotation = 360*2,
 	onComplete = function()  
 		-- fm.FontManager:clearText() 
-		-- str:setText("I like chips\nin\nGravy.\n")
 	end })
 
 transition.to(str2:getView(), { time = t,x = 300, y = 400, alpha = 0.4,xScale = 0.4,yScale = 0.4 })
